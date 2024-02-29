a569870ca9057a919ab3de0023a67b6a0da0a7fe
Seppura-y <y7513414@gmail.com>
2024-02-29 16:39:12 +0800

    1. FolderActivity添加RecyclerView，并显示测试数据

--- app/src/main/java/com/example/donutplayer/FolderActivity.java
+++ app/src/main/java/com/example/donutplayer/FolderActivity.java
@@ -3,11 +3,14 @@ package com.example.donutplayer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.donutplayer.databinding.ActivityFolderBinding;
+
+import java.util.ArrayList;
 
 public class FolderActivity extends AppCompatActivity {
 
@@ -25,6 +28,16 @@ public class FolderActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle(MainActivity.folderList.get(position).getFolderName());
         }
+
+        ArrayList<VideoData> tempList = new ArrayList<>();
+        tempList.add(MainActivity.videoList.get(0));
+        tempList.add(MainActivity.videoList.get(1));
+        tempList.add(MainActivity.videoList.get(2));
+
+        binding.videoRecyclerViewFragmentActivity.setHasFixedSize(true);
+        binding.videoRecyclerViewFragmentActivity.setItemViewCacheSize(10);
+        binding.videoRecyclerViewFragmentActivity.setLayoutManager(new LinearLayoutManager(this));
+        binding.videoRecyclerViewFragmentActivity.setAdapter(new VideoAdapter(this, tempList));
     }
 
     @Override
